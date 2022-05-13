6afb0ba899733a6e8717e28778a3f79528b6aa95
Claudia <claudiaescola7@gmail.com>
2022-05-13 11:26:57 +0100

    brands model updated

--- brands/models.py
+++ brands/models.py
@@ -7,6 +7,7 @@ class Brand (models.Model):
 
     brand_name = models.CharField(max_length=200)
     logo = models.CharField(max_length=4000, null=True, blank=True,)
+    creators_pic = models.CharField(max_length=4000, null=True, blank=True)
     description = models.CharField(max_length=700, null=True, blank=True)
     creators = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=False)
